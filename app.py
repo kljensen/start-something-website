ba62173c59a0e653368b3d00bb33c437c2b3cd73
Kyle L. Jensen <kljensen@gmail.com>
2013-12-22 21:53:31 -0500

    Add boostrap and placeholder templates

--- app.py
+++ app.py
@@ -5,10 +5,16 @@
 """
 
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()

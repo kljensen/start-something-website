90c0131a5663daa67aca477d15cbd15109bafac6
Kyle L. Jensen <kljensen@gmail.com>
2013-12-27 09:13:28 -0500

    Load the class photo carousel via Ajax, to avoid image loading on mobile devices.

--- app.py
+++ app.py
@@ -17,6 +17,23 @@ def index():
     """ Handler for the home page.
     """
 
+    # Render the template and return it in the response.
+    #
+    return render_template(
+        "index.html",
+        instructors=models.instructors,
+        workshops=models.workshops,
+    )
+
+
+@app.route('/carousel')
+def carousel():
+    """ Handler that returns our Bootstrap image carousel
+        as an HTML fragment/partial.  This is loaded by
+        AJAX to prevent the carousel from loading on small
+        devices, when we're not showing it anyway.
+    """
+
     # Get the list of images we want to display in
     # our carousel on the home page. Shuffle them.
     #
@@ -26,12 +43,9 @@ def index():
     # Render the template and return it in the response.
     #
     return render_template(
-        "index.html",
+        "partials/carousel.html",
         carousel_images=carousel_images,
-        instructors=models.instructors,
-        workshops=models.workshops,
     )
-
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ templates/partials/load_carousel.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+  // Are we currently trying to load the carousel?
+  // 
+  var loading = false,
+    loaded = false,
+    holderId = '#carousel-holder';
+
+  // Function that we'll call when the screen is resized
+  // or the page is first loaded.
+  // 
+  function loadCarousel(){
+
+    // Don't try to load the carousel if we're 
+    // already in the process of loading it. Also,
+    // don't load it if we've already loaded it.
+    // Finally, only try to load the carousel if we're
+    // on a screen where it's visible.  In our CSS,
+    // we're hiding it on small screens. Finally,
+    //
+    if (loading === true || loaded === true || $(holderId).is(':visible') === false) {
+      return;
+    }
+
+    // Try to load the carousel. Notice that we're
+    // using jQuery's Promises here (the .done and
+    // .always methods, which are chainable).
+    //
+    loading = true;
+    $.get('/carousel')
+      .done(function(htmlFragment){
+        $(holderId).html(htmlFragment);
+        loaded = true;
+      })
+      .always(function(){loading = false});
+  }
+
+  // Try to load the carousel on window resize.
+  // 
+  $(window).resize(loadCarousel);
+
+  // Try to load the carousel once when the DOM is ready.
+  // 
+  loadCarousel();
+});
